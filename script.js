abf4eedfcd4f225e5d490cdfa96bc1672463a326
GaboProgramers <gutierrezyosguti@gmail.com>
2022-03-25 00:06:46 -0500

    nuevos arreglos y agregando boton de ir arriba

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 }
@@ -14,38 +14,53 @@ window.onscroll = () =>{
 let slides = document.querySelectorAll('.slide-container');
 let index = 0;
 
-function next(){
+function next() {
   slides[index].classList.remove('active');
   index = (index + 1) % slides.length;
   slides[index].classList.add('active');
 }
 
-function prev(){
+function prev() {
   slides[index].classList.remove('active');
   index = (index - 1 + slides.length) % slides.length;
   slides[index].classList.add('active');
 }
 
-document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
-  image_1.addEventListener('click', () =>{
+document.querySelectorAll('.featured-image-1').forEach(image_1 => {
+  image_1.addEventListener('click', () => {
     var src = image_1.getAttribute('src');
     document.querySelector('.big-image-1').src = src;
   });
 });
 
-document.querySelectorAll('.featured-image-2').forEach(image_2 =>{
-  image_2.addEventListener('click', () =>{
+document.querySelectorAll('.featured-image-2').forEach(image_2 => {
+  image_2.addEventListener('click', () => {
     var src = image_2.getAttribute('src');
     document.querySelector('.big-image-2').src = src;
   });
 });
 
-document.querySelectorAll('.featured-image-3').forEach(image_3 =>{
-  image_3.addEventListener('click', () =>{
+document.querySelectorAll('.featured-image-3').forEach(image_3 => {
+  image_3.addEventListener('click', () => {
     var src = image_3.getAttribute('src');
     document.querySelector('.big-image-3').src = src;
   });
 });
 
-//esta es la parte del selector para escribir dentro del carrito de compra
+// boton ir arriba 
 
+$(document).ready(function () {
+    $('.ir-arriba').click(function(){
+      $('body, html').animate({
+        scrollTop: '0px'
+      }, 300);
+
+      $(window).scroll(function(){
+        if ( $(this).scrollTop() > 0  ) {
+            $('.ir-arriba').slideDown(300);
+        } else {
+          $('.ir-arriba').slideUp(300);
+        }
+      });
+    });
+});

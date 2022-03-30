133965483aa97304a3adf20467870b6f4066bede
GaboProgramers <gutierrezyosguti@gmail.com>
2022-03-29 22:45:11 -0500

    actualizacion de js para carrito

--- js/addtoshopping.js
+++ js/addtoshopping.js
@@ -1,36 +1,50 @@
 let allcontainerCart = document.querySelectorAll('.content');
-let arreglo = []
 allcontainerCart.forEach((element)=>{
     element.children[3].addEventListener('click',eventClicked)
 })
+var arreglo = []
 
 function eventClicked(e) {
     e.preventDefault()
     datos = e.target.parentElement
     const dataset = {
-         image : datos.children[0].src,
-         titulo : datos.children[1].textContent,
-         precio : datos.querySelector('.price').textContent,
-         iden : datos.querySelector('a').getAttribute('data-id'),
-         cant: 1
-    }
-    arreglo = [...arreglo,dataset]
-    loadHtml()
-    console.log(dataset)
-}
+        image : datos.children[0].src,
+        titulo : datos.children[1].textContent,
+        precio : datos.querySelector('.price').textContent,
+        iden : datos.querySelector('a').getAttribute('data-id'),
+        cant: 1
+   }
 
 
-function loadHtml() {
-    arreglo.forEach((element)=>{
-        const {image,titulo,precio, iden, cant} = element;
-        const row = document.createElement('div')
-        row.classList.add('content')
-        row.innerHTML = ` 
-        <img src="${image}" alt="">
-        <h3>${titulo}</h3>
-        <div class="price">${precio}</div>
-        <a href="" data-id="${iden}" class="btn btn-add-producto">Comprar Ahora</a>
-        `;
+    var data = {
+        'image': dataset.image,
+        'titulo':dataset.titulo,
+        'precio':dataset.precio,
+        'iden':dataset.iden,
         
-    })
+        
+    
+        
+    }
+
+    arreglo = [...arreglo, data]
+
+    // arreglo[0][0]===data.iden
+
+    if (arreglo.includes(data.iden)){
+        console.log('estamos dentro')
+        
+        if (arreglo.includes(data.iden)) {
+            alert('ya adquiriste este producto dirigite al carro')
+            arr.splice(0)
+
+        }
+    }else{
+        arreglo.unshift(data.iden, data.image, data.titulo, data.precio)
+        console.log('no esta aqui')
+        console.log(arreglo)
+    }
+
+    localStorage.setItem('arreglo',JSON.stringify(arreglo))
+    
 }
--- js/index.js
+++ js/index.js
@@ -1,17 +1,100 @@
-const carrito = new loadHtml();
-let data = document.querySelector('.linke');
-
-
-data.addEventListener('click',() => {
+let dat = document.querySelector('.linke');
+const pr = document.querySelector('.pr')
+const datos = JSON.parse(localStorage.getItem('arreglo'))
+const contenedorCompraDinamica = document.querySelector('.container-sale')
+let btnper =document.querySelector('.btn-pr')
+let arr= []
+let reg = /http/g
+let regExp = new RegExp(reg)
+let cant = ''
+dat.addEventListener('click',() => {
     let nombre = document.querySelector('.nombre').value;
     let telefono = document.querySelector('.telefono').value;
     let direccione = document.querySelector('.direccion').value;
-    let titulo = nombre+' '+telefono+' '+' '+direccione;
+    
+    let camposCompra = ''
+    for (let i of arr){
+        const resultado = regExp.test(i)
+        if (typeof(i)!='object') {
+            if (resultado!= true) {
+                let espacios =i+' '
+                camposCompra += espacios
+            }
+        }
+
+        
+    }
+
+    let titulo = nombre+' '+telefono+'  '+'  '+direccione+'  '+camposCompra+'/'+cant;
 
 
 let direccion = `https://api.whatsapp.com/send?phone=573502117928&text=${titulo}`;
-    data.setAttribute('href',`${direccion}`)
+    dat.setAttribute('href',`${direccion}`)
 })
 
 // carga de archivos en el formulario de compra
+almacen()
+function almacen(e) {
+   
+
+    let maped = Object.entries(datos).map(ke=>{
+        let [keys, value]=ke
+        return value
+})
+
+
+var image = maped.map(function (mape) {
+        if (typeof(mape) != 'object'){
+            arr.unshift(String(mape))
+        }
+        if (typeof(mape)!= 'string'){
+            if (typeof(mape)!='number') {
+                const row = document.createElement('div')
+                const inputs= document.createElement('input')
+                inputs.setAttribute('placeholder','introduce cantidad libra o kilos')
+           
+
+
+              
+                inputs.classList.add('cant-compra')
+                row.classList.add('div-agg')
+                row.innerHTML = `
+                <h1>${mape.titulo}</h1>
+        
+                    <img src="${mape.image}">
+                    <h2>${mape.precio}</h2>
+
+                `
+         
+
+                row.appendChild(inputs)
+                contenedorCompraDinamica.appendChild(row)
+                
+            }
+            
+            
+}})
+btnper.addEventListener('click',()=>{
+
+    const valores = document.getElementsByClassName('cant-compra')
+    for (let i in valores){
+        var resultado = valores[i].value
+        if (resultado != undefined){
+            if (typeof(valores)== 'empty') {
+                alert('llena todos los campos')
+            } else {
+                cant+=resultado+'/'
+            }
+            console.log(resultado)
+        }
+        
+        
+    }
+    dat.classList.add('mostrarBtn')
+    btnper.classList.remove('')
+    
+})
+}
+
+
 
